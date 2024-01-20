18c04873cce03a9819419d257a0dd86ad03bdb98
Rikki Monti <rikkimonti@gmail.com>
2024-01-20 15:58:28 +0500

    feat: implement copyDir function to copy directory content

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const folder = "D:/IT/RS School/HTML-Builder/04-copy-directory/files";
+const destination = "D:/IT/RS School/HTML-Builder/04-copy-directory/files-copy";
+
+function copyDir(src, dest) {
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) return;
+
+    fs.readdir(src, (err, files) => {
+      if (err) return;
+
+      files.forEach(file => {
+        const srcPath = path.join(src, file);
+        const destPath = path.join(dest, file);
+
+        fs.stat(srcPath, (err, stats) => {
+          if (err) return;
+
+          if (stats.isDirectory()) {
+            copyDir(srcPath, destPath);
+          } else {
+            fs.copyFile(srcPath, destPath, (err) => {
+              if (err) return;
+            });
+          }
+        });
+      });
+    });
+  });
+}
+
+copyDir(folder, destination);
