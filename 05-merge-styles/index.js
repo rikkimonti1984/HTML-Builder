5bafca4ccfbe9faad7874d6b85cefbc21be90351
Rikki Monti <rikkimonti@gmail.com>
2024-01-20 16:05:29 +0500

    feat: add script to merge styles into bundle.css

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const folder = "D:/IT/RS School/HTML-Builder/05-merge-styles/styles";
+const destination = "D:/IT/RS School/HTML-Builder/05-merge-styles/project-dist";
+const outputFile = path.join(destination, "bundle.css");
+
+fs.readdir(folder, (_, files) => {
+  let cssFiles = files.filter(file => path.extname(file) === '.css');
+
+  fs.mkdir(destination, { recursive: true }, (_) => {
+    cssFiles.forEach((file, index) => {
+      let filePath = path.join(folder, file);
+      fs.readFile(filePath, 'utf8', (_, data) => {
+        fs.appendFile(outputFile, data + '\n', (_) => {});
+      });
+    });
+  });
+});
