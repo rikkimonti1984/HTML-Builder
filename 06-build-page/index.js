2dab47c3a603a190af37abbd24164360ae02dc95
Rikki Monti <85164568+rikkimonti1984@users.noreply.github.com>
2024-01-25 08:20:01 +0500

    feat: add HTML and assets generation

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -17,3 +17,34 @@ fs.readdir(folder, (_, files) => {
     });
   });
 });
+
+//для html
+const templatePath = './06-build-page/template.html';
+const componentsPath = './06-build-page/components';
+const assetsPath = './06-build-page/assets';
+fs.readFile(templatePath, 'utf8', (_, template) => {
+  fs.readdir(componentsPath, (_, components) => {
+    for (let component of components) {
+      let tag = path.basename(component, '.html');
+      let componentPath = path.join(componentsPath, component);
+
+      fs.readFile(componentPath, 'utf8', (_, componentContent) => {
+        template = template.replace(new RegExp(`{{${tag}}}`, 'g'), componentContent);
+        fs.writeFile(path.join(destination, 'index.html'), template, (_) => {});
+      });
+    }
+  });
+});
+
+//для assets
+const assetsDestination = path.join(destination, 'assets');
+fs.mkdir(assetsDestination, { recursive: true }, (_) => {
+  fs.readdir(assetsPath, (_, files) => {
+    files.forEach(file => {
+      let srcPath = path.join(assetsPath, file);
+      let destPath = path.join(assetsDestination, file);
+
+      fs.copyFile(srcPath, destPath, (_) => {});
+    });
+  });
+});
